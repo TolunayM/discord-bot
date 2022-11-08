b012539d44efcd61c95898f979de10766ba7663c
Tolunay <tlnymtlu@gmail.com>
2022-11-09 00:47:25 +0300

    added new command

---
+++ commands/ascii.js
@@ -0,0 +1,37 @@
+const {SlashCommandBuilder} = require('discord.js');
+
+const ascii = {
+    1: `▒▒▒▒▒▒▐███████▌
+    ▒▒▒▒▒▒▐░▀░▀░▀░▌
+    ▒▒▒▒▒▒▐▄▄▄▄▄▄▄▌
+    ▄▀▀▀█▒▐░▀▀▄▀▀░▌▒█▀▀▀▄
+    ▌▌▌▌▐▒▄▌░▄▄▄░▐▄▒▌▐▐▐▐
+    `,
+    2: `█──██████────██████──█
+    █─██────██──██────██─█
+    ─███─██─██████─██─███
+    ──██────██──██────██
+    ───██████────██████
+    `,
+    3: `╥━━━━━━━━╭━━╮━━┳
+    ╢╭╮╭━━━━━┫┃▋▋━▅┣
+    ╢┃╰┫┈┈┈┈┈┃┃┈┈╰┫┣
+    ╢╰━┫┈┈┈┈┈╰╯╰┳━╯┣
+    ╢┊┊┃┏┳┳━━┓┏┳┫┊┊┣
+    ╨━━┗┛┗┛━━┗┛┗┛━━┻
+    `,
+    4: `¯\_( ͡❛ ͜ʖ ͡❛)_/¯`
+}
+
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('ascii')
+        .setDescription("Random ascii"),
+    async execute(interaction){
+        
+        const rand = Math.floor(Math.random() * 4) + 1;
+        console.log(ascii,rand);
+        await interaction.reply(`${ascii[rand]}`); 
+        }
+};
